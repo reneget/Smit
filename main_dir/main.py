e240a88ab710909b43aa2e86326d251e16e36021
reneget_ <ser200512272@gmail.com>
2023-07-01 03:43:19 +0700

    дописал функцию проверки нахождения команды, сказанной пользователем, в базе данных команд (всё в main)

--- main_dir/main.py
+++ main_dir/main.py
@@ -2,8 +2,18 @@ from tts import tts_func
 from stt import stt_funk
 from commands import *
 from time import sleep as wait
+import pandas as pd
 
 while True:
     text = stt_funk()
     if 'скай' in text:
         tts_func('Слушаю вас')
+        command_text = stt_funk()
+        connection = sqlite3.connect('commands_database.db')
+        query = f"SELECT * FROM commands_table WHERE user_title = '{command_text}'"
+        df = pd.read_sql_query(query, connection)
+        connection.close()
+        if df.empty:
+            tts_func(f'Вы сказали, {command_text}, но в моём списке комманд такой нет. Внесите её сами или идите нах')
+        else:
+            pass
