ba3489c5fe60e72dbc4a0f66cd69497c6a4d0383
reneget <ser20051227@gmail.com>
2024-03-28 21:55:07 +0500

    complete AI tts function

--- tts_AI.py
+++ tts_AI.py
@@ -1,17 +1,16 @@
-from elevenlabs import Voice, VoiceSettings, play
+from elevenlabs import play, VoiceSettings
 from elevenlabs.client import ElevenLabs
-
-
-play(audio)
 
 client = ElevenLabs(
     api_key="78041bd5c835902c252e9252489cd8fe"  # Defaults to ELEVEN_API_KEY
 )
 
-audio = client.generate(
-    text="Hello!",
-    voice="Rachel",
-    model="eleven_multilingual_v2"
-)
 
-play(audio)
+def ai_tts(text):
+    audio = client.generate(
+        text=text,
+        voice="Bill",
+        model="eleven_multilingual_v2",
+        voice_settings=VoiceSettings(stability=0.01, similarity_boost=1.0, style=0.0, use_speaker_boost=True)
+    )
+    play(audio)
