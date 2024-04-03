3a4ede607d28daff7a131cb8ff02efee8ad58f03
Maxima <119327749+qMatsushima@users.noreply.github.com>
2024-04-03 19:33:35 +0500

    Add files via upload

--- UI on Flet.py
+++ UI on Flet.py
@@ -1,22 +1,40 @@
 import flet as ft
-import flet_core
+import csv
+import math
 
 
 def ui_main(page):
 
-    def add_command(e):
-        pass
+    label_field = ft.TextField(label='Введите команду', border_color='#DFB6B2', hint_text='id/user/tech/link/time')
 
-    def validate(e):
-        if btn_add_command:
-            pass
+    def add_command(e):     # добавляем
+        get_value = label_field.value.split()
+        page.update()
+        with open('commands_data.csv', 'a', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            writer.writerow(get_value)
+        print('добавлено')
+        print(get_value, type(get_value))
 
-    def change_theme(e):
+
+    def del_command(e):     # удаляем
+        get_value = label_field.value.split()
+        with open('commands_data.csv', 'r', newline='', encoding='utf-8') as csvfile:
+            rows = list(csv.reader(csvfile))
+        with open('commands_data.csv', 'w', newline='', encoding='utf-8') as csvfile:
+            writer = csv.writer(csvfile)
+            for row in rows:
+                if row[0].strip() != get_value[0]:
+                    writer.writerow(row)
+        print('удалено')
+
+
+    def change_theme(e):    # смена темы
         page.theme_mode = 'light' if page.theme_mode == 'dark' else 'dark'
         page.update()
 
 
-    # colors
+    # clr colors
     dark_grey = '#11212D'
     grey = '#253745'
     light_grey = '#4A5C6A'
@@ -27,95 +45,114 @@ def ui_main(page):
     skin = '#FBE4D8'
     used = '#F4ECF7'
 
-
     page.title = 'Smith'
     page.theme_mode = 'light'
     page.vertical_alignment = ft.MainAxisAlignment.START
     page.window_width = 358         # 350
     page.window_height = 406        # 350
-    # page.window_resizable = False
+    page.window_resizable = False
 
-    btn_add_com = ft.ElevatedButton(text="Добавить")
-    btn_del_com = ft.ElevatedButton(text="Удалить")
-    label_field = ft.TextField(label='Введите команду', border_color=sand)
+    btn_add_com = ft.ElevatedButton(text="Добавить", on_click=add_command)
+    btn_del_com = ft.ElevatedButton(text="Удалить", on_click=del_command)
     btn_sunny = ft.IconButton(ft.icons.SUNNY, icon_color='PURPLE', on_click=change_theme)
     space = ft.IconButton(ft.icons.MENU, icon_color='PURPLE')
 
     page.add(
-
-        ft.Container(
-            width=320,
-            height=220,
-            padding=10,
-            bgcolor=sand,
-            border_radius=10,
-            content=ft.Column(
-                controls=[
-                    ft.Row(
-                        [
-                            ft.Container(
-                                width=300,
-                                height=65,
-                                padding=15,
-                                bgcolor='#F4ECF7',
-                                border_radius=10,
-                                content=ft.Row(
-                                    [
-                                        space,
-                                        ft.Text('Settings', size=26, color='#2C3E50'),
-                                        btn_sunny
-                                    ],
-                                    alignment=ft.MainAxisAlignment.SPACE_BETWEEN
-                                )
-                            ),
-                        ]
-                    ),
-
-
-                    ft.Column(
-                        [
-                            ft.Checkbox(label='Озвучка помощника'),
-                            ft.Checkbox(label='Ставь лайк'),
-                            ft.Checkbox(label='Пнуть ветер'),
-                        ],
-                        alignment=ft.MainAxisAlignment.CENTER
-                    )
-                ]
-            )
-        ),
-
-        ft.Container(
-            width=320,
-            height=76,
-            padding=10,
-            bgcolor='dark',
-            border_radius=10,
-            content=ft.Column(
-                controls=[
-                    ft.Row(
-                        [
-                            label_field
-                        ],
-                        alignment=ft.MainAxisAlignment.CENTER
-                    )
-                ]
-            )
-
-        ),
-
-        ft.Row(
+        ft.Column(
             [
-                btn_del_com,
-                btn_add_com,
+                ft.Container(
+                    alignment=ft.alignment.center,
+                    gradient=ft.LinearGradient(
+                        begin=ft.alignment.top_left,
+                        end=ft.Alignment(0.8, 1),
+                        colors=[
+                            "0xffFFC7B4",
+                            "0xffFFA080",
+                        ],
+                        tile_mode=ft.GradientTileMode.MIRROR,
+                        rotation=math.pi / 3,
+                    ),
+                    width=320,
+                    height=220,
+                    padding=10,
+                    bgcolor=sand,
+                    border_radius=10,
+                    content=ft.Column(
+                        controls=[
+                            ft.Row(
+                                [
+                                    ft.Container(
+                                        width=300,
+                                        height=65,
+                                        padding=15,
+                                        bgcolor='#F4ECF7',
+                                        border_radius=10,
+                                        content=ft.Row(
+                                            [
+                                                space,
+                                                ft.Text('Settings', size=26, color='#2C3E50'),
+                                                btn_sunny
+                                            ],
+                                            alignment=ft.MainAxisAlignment.SPACE_BETWEEN
+                                        )
+                                    ),
+                                ]
+                            ),
+
+
+                            ft.Column(
+                                [
+                                    ft.Checkbox(label='Озвучка помощника'),
+                                    ft.Checkbox(label='Функция'),
+                                    ft.Checkbox(label='Функция'),
+                                ],
+                                alignment=ft.MainAxisAlignment.CENTER
+                            )
+                        ]
+                    )
+                ),
+
+                ft.Container(
+                    width=320,
+                    height=76,
+                    padding=10,
+                    bgcolor='dark',
+                    border_radius=10,
+                    content=ft.Column(
+                        controls=[
+                            ft.Column(
+                                [
+                                    label_field,
+                                    ft.Row(
+                                        [
+                                            btn_del_com,
+                                            btn_add_com,
+                                        ],
+                                        alignment=ft.MainAxisAlignment.SPACE_EVENLY
+                                    )
+                                ],
+                                alignment=ft.MainAxisAlignment.CENTER
+                            )
+                        ]
+                    )
+
+                ),
+
+                ft.Row(
+                    [
+                        btn_del_com,
+                        btn_add_com,
+                    ],
+                    alignment=ft.MainAxisAlignment.SPACE_EVENLY
+                )
             ],
-            alignment=ft.MainAxisAlignment.SPACE_EVENLY
-        )
+            alignment=ft.MainAxisAlignment.CENTER
+        ),
     )
 
 
+
 ft.app(target=ui_main)
-
-
 
 
 
